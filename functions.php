9c75bfbddfa367b02514b4b04bf0a8cebb433010
bcampeau <brad@alleyinteractive.com>
2016-10-03 16:55:28 -0400

    Fixed PHPCS reported issues

--- functions.php
+++ functions.php
@@ -1,10 +1,24 @@
 <?php
-function standards_example_function($color) {
-	return get_posts(array(
+/**
+ * The base customizations for this theme.
+ *
+ * Holds a few custom functions intended solely for the LoopConf code review demo.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Returns all posts that are not tagged with the given color.
+ *
+ * @param string $color The color for the query.
+ * @return array
+ */
+function standards_example_function( $color ) {
+	return get_posts( array(
 		'meta_query' => array(
 			array(
 				'key' => 'color',
-				'value' =>  $color,
+				'value' => $color,
 				'compare' => 'NOT LIKE',
 			),
 		),
