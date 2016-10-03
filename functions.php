2920d4ca9eb9c63214975421c0b72210f92810f6
bcampeau <brad@alleyinteractive.com>
2016-10-03 16:32:28 -0400

    Fixes for PHPCS standards

--- functions.php
+++ functions.php
@@ -1,10 +1,16 @@
 <?php
-function standards_example_function($color) {
-	return get_posts(array(
+/**
+ * Returns all posts that are not tagged with the given color.
+ *
+ * @param string $color
+ * @return array
+ */
+function standards_example_function( $color ) {
+	return get_posts( array(
 		'meta_query' => array(
 			array(
-				'key'     => 'color',
-				'value'   =>  $color,
+				'key' => 'color',
+				'value' =>  $color,
 				'compare' => 'NOT LIKE',
 			),
 		),
